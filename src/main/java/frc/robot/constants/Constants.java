646ae966193cedeaadf474a4a58feffd206323a6
AnthonyFurman <71467649+AnthonyFurman@users.noreply.github.com>
2022-12-01 15:17:19 -0800

    replace pathweaver with pathplanner, add some constants

--- src/main/java/frc/robot/constants/Constants.java
+++ src/main/java/frc/robot/constants/Constants.java
@@ -3,6 +3,15 @@ package frc.robot.constants;
 public final class Constants {
   public static final double GRAVITATIONAL_ACCEL = 9.8;
   public static final double kMaxVoltage = 12.0;
+  public static final double kLoopTime = 0.02;
+  
+  public static final double kCANcoderResolution = 4096;
+  public static final double kFalconResolution = 2048;
+  public static final double kFalconMaxRPM = 6380.0;
+
+  // CAN bus names
+  // public static final String kCanivoreCAN = ""; // replace this if using CANivore
+  public static final String kRioCAN = "rio";
 
   public static final OIConstants oi = new OIConstants();
   public static final DriveConstants drive = new DriveConstants();
